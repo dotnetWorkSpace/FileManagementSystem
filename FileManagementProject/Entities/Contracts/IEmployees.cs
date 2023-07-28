20fbbd85966a3aa0181ff7147a7fc237ab3fc5b7
Beyza Yüksel <beyzayuksel0111@gmail.com>
2023-07-28 11:46:50 +0300

    Interface kısmı

---
+++ FileManagementProject/Entities/Contracts/IEmployees.cs
@@ -0,0 +1,11 @@
+﻿namespace FileManagementProject.Entities.Contracts
+{
+    public interface IEmployees
+    {
+        
+        Result<Employees> GetOneEmployee(int id); //düzeltilmesi gerekiyor -Result
+        List<Employees> Get();
+
+
+    }
+}
